3cc2a4c022d402387caf94d5b4c81556d05ac2d4
Valter Lafuente Junior <valterlafuente@yahoo.com.br>
2020-04-27 17:10:04 -0300

    troca de println para sp.append

--- src/trab1_compactador/Compacta_e_Descompacta.java
+++ src/trab1_compactador/Compacta_e_Descompacta.java
@@ -5,13 +5,11 @@
  */
 package trab1_compactador;
 
-import com.sun.xml.internal.ws.util.StringUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import jdk.nashorn.internal.runtime.JSType;
 
 /**
  *
@@ -23,7 +21,8 @@ public class Compacta_e_Descompacta {
         ListaEncadeada lista = new ListaEncadeada();
         compacta(lista);
         descompacta(lista);
-        System.out.println();
+      
+        
     }
 
     static void compacta(ListaEncadeada lista) {
@@ -43,10 +42,10 @@ public class Compacta_e_Descompacta {
                         lista.insereInicio(palavras[i]);
                     } else {
                         linha = linha.replace(palavras[i], String.valueOf(lista.posicaoNo(palavras[i])));
-                    }
+                    }    
+                    
+                   sb.append(palavras[i]+"\n");
                 }
-                System.out.println(linha);
-                
             }
             sb.append("0");
             fr.close();
@@ -64,9 +63,10 @@ public class Compacta_e_Descompacta {
             escritor.append(conteudo);
             escritor.close();
             if (compacta) {
-                System.out.println("Arquivo compactado!");
-            } else {
                 System.out.println("Arquivo descompactado!");
+            } else {
+                System.out.println("Arquivo compactado!");
+             
             }
         } catch (IOException ex) {
             System.out.println("ocorreu um erro ao escrever arquivo!");
@@ -95,10 +95,9 @@ public class Compacta_e_Descompacta {
                         linha = linha.replace(palavras[i], lista.PalaravaPosicaoNo(Integer.parseInt(palavras[i])));
                     }
                 }
-
-                System.out.println(linha);
-              
+                  sb.append(palavras[j]+"\n");
             }
+             sb.append("0");
             fr.close();
             EscreveArquivo("descompactado", sb, false);
         } catch (IOException e) {
